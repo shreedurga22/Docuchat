6a45c2f77fbe887ff9e1ccb1f10e82f925ebed45
Shree Durga <shreedurga114@gmail.com>
2025-09-01 21:02:14 +0530

    Apply .gitignore cleanup

---
+++ TranslationTools/TranslationTools/translate.py
@@ -0,0 +1,10 @@
+﻿import sys
+from googletrans import Translator
+
+# Configure UTF-8 output
+sys.stdout.reconfigure(encoding='utf-8')
+
+# Translate and print
+translator = Translator()
+translation = translator.translate("Hello", dest='ta').text
+print(translation)
---
+++ translate.py
@@ -0,0 +1,8 @@
+﻿import sys
+from googletrans import Translator
+
+# Force UTF-8 output
+sys.stdout.reconfigure(encoding='utf-8')
+
+translator = Translator()
+print(translator.translate('Hello', dest='ta').text)
